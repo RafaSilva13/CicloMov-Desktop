0c99a2126da8d354d4624899e01c8400cc14aa61
Goncalves-Victor <victoremanuel.psi@gmail.com>
2022-12-23 21:40:17 -0300

    commit

--- CicloMov Web/criarServico.php
+++ CicloMov Web/criarServico.php
@@ -6,11 +6,6 @@ session_start();
 $id = $_POST['idPonto'];
 $tempo = $_POST['time'] . ':00';
 
-// $_SESSION["tempo"] = $tempo;
-// date_default_timezone_set('America/Sao_Paulo');
-// $hora = date('H:i:s');
-// $v = explode(':', $_SESSION['tempo']);
-// $_SESSION['tempo_restante'] = date('H:i:s', strtotime("{$hora} + {$v[0]} hours {$v[1]} minutes {$v[2]} seconds"));
 
 $user = $_SESSION['login'];
 
@@ -22,6 +17,13 @@ if (!$conn) {
 $sql = "INSERT INTO servicos (id_clientes, id_ponto, tempo_permanencia) VALUES ('$user', '$id', '$tempo')";
 
 
+if (mysqli_query($conn, $sql)) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+
+$sql = "UPDATE clientes SET tempo_geral_clientes='$tempo' WHERE cod_clientes = '$cod'";
 if (mysqli_query($conn, $sql)) {
     echo "New record created successfully";
 } else {
